03347868624ead7c8b95c5899f72f05e32cc760b
12jaime12 <12jaimejcc12@gmail.com>
2023-06-06 15:07:56 +0200

    iteracion 1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="logoVirtualSchool">
+          <img
+            src="https://res.cloudinary.com/dtyjzv2xg/image/upload/v1686055967/vslogo1_fsrfl1.png"
+            alt="logo"
+            className="logo"
+          />
+        </div>
+      </header>
+    </>
+  );
+};
