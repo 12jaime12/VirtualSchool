68c3a9c9be423d2ccf356b91a85593b345a6b21e
andreafcb1 <andrea_garcillan@hotmail.com>
2023-06-06 17:15:16 +0200

    checkcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return <></>;
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/pages/CheckCode/CheckCode.jsx
+++ src/pages/CheckCode/CheckCode.jsx
@@ -1,7 +1,61 @@
 import "./CheckCode.css";
 
 const CheckCode = () => {
-  return <div>CheckCode</div>;
+
+
+  return (
+  <>
+      <div className="form-wrap">
+        <h1>Verifica tu código de registro</h1>
+        <p>Escribe el código enviado a tu correo electrónico</p>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="user_container form-group">
+            <input
+              className="input_user"
+              type="text"
+              id="name"
+              name="name"
+              autoComplete="false"
+              {...register("confirmationCode", { required: false })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              Código de registro
+            </label>
+          </div>
+
+          <div className="btn_container">
+            <button
+              id="btnCheck"
+              className="btn"
+              type="submit"
+              disabled={send}
+              style={{ background: send ? "#49c1a388" : "#49c1a2" }}
+            >
+              Código de verificación
+            </button>
+          </div>
+          <div className="btn_container">
+            <button
+              id="btnResend"
+              className="btn"
+              disabled={send}
+              style={{ background: send ? "#49c1a388" : "#49c1a2" }}
+              onClick={() => handleReSend()}
+            >
+              Reenviar código
+            </button>
+          </div>
+
+          <p className="bottom-text">
+            <small>
+            Si el código introducido no es correcto ❌, su usuario será eliminado de la
+               base de datos y tendrá que registrarse de nuevo.{" "}
+            </small>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default CheckCode;
