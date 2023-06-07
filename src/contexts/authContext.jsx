21f32209115e31615df278014e20782b4c5e9012
albertosantfaj <alberto.sant.faj@hotmail.com>
2023-06-07 16:35:06 +0200

    proyecto

---
+++ src/contexts/authContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  //--------------------Login---------------------------
+
+  const userlogin = (data) => {
+    localStorage.setItem("user", data);
+    const parseData = JSON.parse(data);
+    setUser(() => parseData);
+  };
+};
