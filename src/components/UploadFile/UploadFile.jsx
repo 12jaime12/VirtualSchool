4a3c01eacc589faaa8e2c06c706b1272017cd12b
andreafcb1 <andrea_garcillan@hotmail.com>
2023-06-08 15:15:57 +0200

    css

--- src/components/UploadFile/UploadFile.jsx
+++ src/components/UploadFile/UploadFile.jsx
@@ -84,12 +84,12 @@ const Uploadfile = ({ registerForm }) => {
         <img id="file-image" src="#" alt="Preview" className="hidden" />
         <div id="start">
           <i className="fa fa-download" aria-hidden="true"></i>
-          <div className="divSelect">Select a file or drag here</div>
+          <div className="divSelect">Seleccione un archivo o arrastre aquí</div>
           <div id="notimage" className="hidden">
-            Please select an image
+            Por favor, seleccione una imagen
           </div>
           <span id="file-upload-btn" className="btn btn-primary">
-            Select a file
+            Seleccione un archivo
           </span>
         </div>
         <div id="response" className="hidden">
