2a4ae44c455f80cdea6e98cc06d187917833a5cd
Laura Martínez <you@example.com>
2023-06-09 16:27:00 +0200

    cambiohorario

--- src/components/HorarioEscolar/Horario3/Horario3.jsx
+++ src/components/HorarioEscolar/Horario3/Horario3.jsx
@@ -21,131 +21,131 @@ const Horario3 = () => {
           <tr>
             <td className="horas">8:30 a 9:25 </td>
             <td className="Lengua">
-              Lengua*<span> Aula</span>
+              Lengua<span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula</span>
+              Mates <span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>{" "}
+              Mates <span> Aula </span>{" "}
             </td>
             <td className="Edfisica">
-              Educación física*<span> Gimnasio</span>{" "}
+              Educación física<span> Gimnasio</span>{" "}
             </td>
           </tr>
           <tr>
             <td className="horas">9:25 a 10:20 </td>
             <td className="Ingles">
-              Inglés*<span> Aula</span>
+              Inglés<span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Musica">
-              Musica *<span> Aula musica</span>
+              Musica <span> Aula musica</span>
             </td>
             <td className="Ingles">
-              Inglés*<span> Aula</span>
+              Inglés<span> Aula</span>
             </td>
             <td className="Lengua">
-              Lengua *<span> Aula</span>
+              Lengua <span> Aula</span>
             </td>
           </tr>
           <tr>
             <td className="horas">10:20 a 11:10 </td>
             <td className="Lengua">
-              Lengua*<span> Aula</span>
+              Lengua<span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Lengua">
-              Lengua*<span>Aula</span>
+              Lengua<span>Aula</span>
             </td>
             <td className="Ingles">
-              Inglés *<span> Aula</span>
+              Inglés <span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
           </tr>
           <tr>
             <td className="horas">11:10 a 11:35 </td>
             <td className="recreo">
-              Recreo *<span> Patio</span>
+              Recreo <span> Patio</span>
             </td>
             <td className="recreo">
-              Recreo *<span> Patio</span>
+              Recreo <span> Patio</span>
             </td>
             <td className="recreo">
-              Recreo *<span> Patio</span>
+              Recreo <span> Patio</span>
             </td>
             <td className="recreo">
-              Recreo *<span> Patio</span>
+              Recreo <span> Patio</span>
             </td>
             <td className="recreo">
-              Recreo *<span> Patio</span>
+              Recreo <span> Patio</span>
             </td>
           </tr>
           <tr>
             <td className="horas">11:35 a 12:30 </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Musica">
-              Música *<span> Aula música</span>
+              Música <span> Aula música</span>
             </td>
             <td className="Lengua">
-              Lengua *<span> Aula</span>
+              Lengua <span> Aula</span>
             </td>
             <td className="Tecno">
               {" "}
-              Tecnología *<span> Info2</span>
+              Tecnología <span> Info2</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
           </tr>
           <tr>
             <td className="horas">12:30 a 13:25 </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Tecno">
               {" "}
-              Tecnología *<span> Info2</span>
+              Tecnología <span> Info2</span>
             </td>
             <td className="Lengua">
-              Lengua *<span> Aula</span>
+              Lengua <span> Aula</span>
             </td>
             <td className="Tecno">
               {" "}
-              Tecnología *<span> Info2</span>
+              Tecnología <span> Info2</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
           </tr>
           <tr>
             <td className="horas">13:25 a 14:15</td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Tecno">
               {" "}
-              Tecnología *<span> Info2</span>
+              Tecnología <span> Info2</span>
             </td>
             <td className="Lengua">
-              Lengua *<span> Aula</span>
+              Lengua <span> Aula</span>
             </td>
             <td className="Mates">
-              Mates *<span> Aula </span>
+              Mates <span> Aula </span>
             </td>
             <td className="Lengua">
-              Lengua*<span> Info 2</span>
+              Lengua<span> Info 2</span>
             </td>
           </tr>
         </tbody>
